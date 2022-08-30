9f788451d9f4c47d83ed0d738cec80cdd8d33480
David Villanueva <davidvillanuevamarzo@gmail.com>
2022-08-30 12:37:54 +0200

    89. LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGIFResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class GifsService {
   private _historial: string[] = [];
 
   // TODO: Cambiar esto por su tipado
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
@@ -24,16 +25,29 @@ export class GifsService {
     if( !this._historial.includes( query ) )  {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify( this._historial ));
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=aPhQbEZKb381egJq0ZwQAIb9RfZHBBd3&q=${ query }&limit=10`)
-    .subscribe( (resp : any) => {
+    this.http.get<SearchGIFResponse>(`https://api.giphy.com/v1/gifs/search?api_key=aPhQbEZKb381egJq0ZwQAIb9RfZHBBd3&q=${ query }&limit=10`)
+    .subscribe( (resp ) => {
       console.log( resp.data );
       this.resultados = resp.data;
+      localStorage.setItem('resultados', JSON.stringify(this.resultados));
     })
     
     console.log(this._historial);
   }
 
-  constructor( private http: HttpClient) { }
+  constructor( private http: HttpClient) { 
+
+    if (localStorage.getItem('historial') ) {
+      this._historial= JSON.parse(localStorage.getItem('historial')! );
+    }
+
+    if (localStorage.getItem('resultados') ) {
+      this.resultados= JSON.parse(localStorage.getItem('resultados')! );
+    }
+
+  }
 }
