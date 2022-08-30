2efcad8a54efdfe4e9d230f25eea272f2020947f
David Villanueva <davidvillanuevamarzo@gmail.com>
2022-08-30 11:14:11 +0200

    86. Mostrar resultados en pantalla

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,16 +6,34 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 
+  private apiKey: string= 'aPhQbEZKb381egJq0ZwQAIb9RfZHBBd3';
+
   private _historial: string[] = [];
+
+  // TODO: Cambiar esto por su tipado
+  public resultados: any[] = [];
 
   get historial() {
     return [...this._historial];
   }
 
   buscarGifs( query: string) {
-    this._historial.unshift(query);
+
+    query = query.trim().toLowerCase();
+  
+    if( !this._historial.includes( query ) )  {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+    }
+
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=aPhQbEZKb381egJq0ZwQAIb9RfZHBBd3&q=${ query }&limit=10`)
+    .subscribe( (resp : any) => {
+      console.log( resp.data );
+      this.resultados = resp.data;
+    })
+    
     console.log(this._historial);
   }
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
 }
